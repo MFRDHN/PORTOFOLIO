26df681000e9e7144e9f2f4f8bc98b580c10b4e3
MFRDHN1 <muhammadfajrirmdhn27@gmail.com>
2026-01-06 21:16:41 +0700

    chore: force re-upload assets with new name

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -133,7 +133,7 @@ export default function Hero() {
         <div className="relative h-full flex items-end justify-center">
           <div className="absolute bottom-0 w-[100%] h-[30%] bg-red-600/20 blur-[120px] rounded-full" />
           <Image
-            src="/aset/hero.png" 
+            src="/aset/hero_profile.png" 
             alt="Avatar" 
             width={1400} 
             height={1400}
